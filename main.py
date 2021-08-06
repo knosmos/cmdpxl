66ccbe66fc80c17cd6ef30b170cce22c209bd238
Jieruei Chang <30610197+knosmos@users.noreply.github.com>
2021-08-06 16:01:07 -0400

    Graphics overhaul
    improve terminal colors, rounded containers for color selection and image box

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import sys # Write to terminal
 import os
 import numpy as np
 
+''' DISPLAY PARAMS '''
+highlight_color = [214, 39, 112]
+secondary_color = [53, 204, 242]
+edge_color = [77, 77, 77]
+padding_x = 1
+padding_y = 1
+
 ''' INPUT '''
 # Define the getch function used to get keyboard input
 # https://stackoverflow.com/a/47548992
@@ -47,30 +54,50 @@ def show_cursor():
 ''' IMAGE DRAWING '''
 
 # Prints text at x, y with background color
-def draw(color, x, y, text):
+def draw(color, x, y, text, textcolor = None):
     # ANSI Escape sequences
     # Somehow this was easier than curses or rich
     
     # Make text color black if background is too light
-    if sum(color) > 350:
+    if sum(color) > 350 and textcolor == None:
         text = f"\x1b[38;2;0;0;0m{text}\x1b[0m"
+    if textcolor != None:
+        text = f"\x1b[38;2;{textcolor[0]};{textcolor[1]};{textcolor[2]}m{text}\x1b[0m"
 
-    color_start = f"\x1b[48;2;{color[0]};{color[1]};{color[2]}m"
-    color_end = "\x1b[0m"
+    if color[0] != -1: # transparent
+        color_start = f"\x1b[48;2;{color[0]};{color[1]};{color[2]}m"
+        color_end = "\x1b[0m"
+    else:
+        color_start = ""
+        color_end = ""
     position_start = f"\x1b7\x1b[{y};{x}f"
     position_end = "\x1b8"
     sys.stdout.write(color_start+position_start+text+position_end+color_end)
 
-# Prints an image
-def draw_image(img,pos,offset_y=4):
+# Draws the image box
+def draw_image_box(img):
+    offset_y = 6
     y, x, _ = img.shape
+    box_top = "╭"+"─"*(x*2)+"╮"
+    box_mid = "│"+" "*(x*2)+"│"
+    box_bot = "╰"+"─"*(x*2)+"╯"
+    draw([-1],1+padding_x,offset_y+padding_y,box_top,edge_color)
+    for i in range(y):
+        draw([-1],1+padding_x,offset_y+padding_y+1+i,box_mid,edge_color)
+    draw([-1],1+padding_x,offset_y+padding_y+1+y,box_bot,edge_color)
+
+# Prints an image
+def draw_image(img, pos):
+    offset_y = 6
+    y, x, _ = img.shape
+
     for j in range(y):
         for i in range(x):
             if i == pos[0] and j == pos[1]:
                 text = "[]"
             else:
                 text = "  "
-            draw(img[j][i], i*2+1, j+1+offset_y, text)
+            draw(img[j][i], i*2+2+padding_x, j+1+offset_y+padding_y, text)
     sys.stdout.flush()
 
 ''' COLOR SELECTION '''
@@ -86,7 +113,22 @@ def hsv_to_rgb(color):
 
 # Draws the color selection display
 def color_select(color,offset_y=1):
-    draw([0,0,0],1,1+offset_y,"[u/j]: hue  [i/k]: sat  [o/l]: val")
+    # Draw the edge box
+    section_width = 11
+    box_height = 3
+    box_top = "╭"+"┬".join(["─"*section_width]*4)+"╮"
+    box_mid = "│"+"│".join([" "*section_width]*4)+"│"
+    box_bot = "╰"+"┴".join(["─"*section_width]*4)+"╯"
+
+    draw([-1],1+padding_x,1+offset_y+padding_y,box_top,edge_color)
+    for i in range(box_height):
+        draw([-1],1+padding_x,1+offset_y+padding_y+1+i,box_mid,edge_color)
+    draw([-1],1+padding_x,1+offset_y+padding_y+box_height,box_bot,edge_color)
+
+    # Draw instruction text
+    instructions = ["[u/j]: hue","[i/k]: sat", "[o/l]: val", "current"]
+    for i in range(len(instructions)):
+        draw([-1],1+padding_x+1+i*12,1+offset_y+padding_y+1,instructions[i],secondary_color)
 
     ticks = 10
     # Draw hue display
@@ -99,7 +141,7 @@ def color_select(color,offset_y=1):
             text = "●"
         else:
             text = " "
-        draw(ncolor_rgb,h//(180//ticks)+1, 2+offset_y,text)
+        draw(ncolor_rgb, h//(180//ticks)+1+padding_x+1, 2+offset_y+padding_y+1, text)
 
     # Draw sat display
     for s in range(0,251,250//ticks):
@@ -111,7 +153,7 @@ def color_select(color,offset_y=1):
             text = "●"
         else:
             text = " "
-        draw(ncolor_rgb,s//(250//ticks)+ticks+3,2+offset_y,text)
+        draw(ncolor_rgb, s//(250//ticks)+ticks+3+padding_x+1, 2+offset_y+padding_y+1, text)
 
     # Draw val display
     for v in range(0,251,250//ticks):
@@ -123,10 +165,10 @@ def color_select(color,offset_y=1):
         else:
             text = " "
             #print(hsv_color[2])
-        draw(ncolor_rgb,v//(250//ticks)+2*ticks+5,2+offset_y,text)
+        draw(ncolor_rgb,v//(250//ticks)+2*ticks+5+padding_x+1,2+offset_y+padding_y+1,text)
     
     # Draw current color
-    draw(hsv_to_rgb(hsv_color),37,2+offset_y,"[current]")
+    draw(hsv_to_rgb(hsv_color),37+padding_x+1,2+offset_y+padding_y+1," "*11)
 
 #color_select([255,200,200])
 
@@ -155,7 +197,7 @@ def change_value(color, amount):
 def main():
     
     clear()
-    draw([82, 7, 52], 1, 1, "CMDPXL - A TOTALLY PRACTICAL IMAGE EDITOR")
+    draw([-1], 1, 1, "CMDPXL - A TOTALLY PRACTICAL IMAGE EDITOR", highlight_color)
     print()
     print("[O]: Open file")
     print("[C]: Create new file")
@@ -181,11 +223,20 @@ def main():
     hide_cursor()
     pos = [0,0]
     color = [0,0,250]
+    draw_image_box(img)
+
+    dimensions = os.get_terminal_size()
 
     while True:
-        draw([82, 7, 52], 1, 1, f"CMDPXL: {filename} ({img.shape[1]}x{img.shape[0]})")
+        # Repaint on window size change
+        if os.get_terminal_size() != dimensions:
+            clear()
+            dimensions = os.get_terminal_size()
+            draw_image_box(img)
+
+        draw([-1], 1+padding_x, 1+padding_y, f"CMDPXL: {filename} ({img.shape[1]}x{img.shape[0]})", highlight_color)
         color_select(color)
-        draw([0,0,0], 1, 4, "[WASD]: move  [e]: draw  [esc]: quit")
+        draw([-1], 1+padding_x, 8+padding_y+img.shape[0], "[wasd]: move  [e]: draw  [esc]: quit", secondary_color)
         draw_image(img,pos)
 
         m = getch()
@@ -219,7 +270,7 @@ def main():
         if m == "\x1b": # esc
             show_cursor()
             clear()
-            draw([82, 7, 52], 1, 1, "QUIT")
+            draw([-1], 1, 1, "QUIT", highlight_color)
             print()
             print("[S]: Save and exit")
             print("[Q]: Quit without saving")
@@ -236,4 +287,5 @@ def main():
             elif option == "q":
                 exit()
             hide_cursor()
+            draw_image_box(img)
 main()
